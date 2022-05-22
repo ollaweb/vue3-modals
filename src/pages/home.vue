7c104316e3c58f933d5a34ffd2faacc45fd59b81
Olga L <olla.bradford@gmail.com>
2022-05-22 21:39:00 +0300

    Init. Created Simple Modal Window. Added Vue Transition

---
+++ src/pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="btn btnPrimary" @click="simpleModal = !simpleModal">
+    Show Simple Modal
+  </button>
+  <Modal
+    v-show="simpleModal"
+    title="Simple Modal Window"
+    @close="simpleModal = false"
+  >
+    <template v-slot:body>
+      <p>Congratulations! You opened Simple Modal Window!</p>
+      <br />
+      <button class="btn btnPrimary" @click="simpleModal = !simpleModal">
+        Well done!
+      </button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from '@/components/UI/Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      simpleModal: false
+    }
+  }
+}
+</script>
