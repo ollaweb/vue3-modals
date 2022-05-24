64d3fbfaaf621c1a3fe14176a2d48a63dc26bfda
Olga L <olla.bradford@gmail.com>
2022-05-24 13:49:13 +0300

    Added validation to Sign Up; added Thanks modal

--- src/pages/logIn.vue
+++ src/pages/logIn.vue
@@ -14,6 +14,7 @@
             Email is't correct!
           </p>
           <input
+            :class="{ error: v$.login.email.$error }"
             v-model="login.email"
             type="email"
             name="login"
@@ -31,6 +32,7 @@
           </p>
 
           <input
+            :class="{ error: v$.login.password.$error }"
             v-model="login.password"
             type="password"
             name="password"
@@ -41,7 +43,7 @@
         <button class="btn btnPrimary">Log in</button>
         <div class="form-notes">
           <p>Don't have an account yet?</p>
-          <p @click="onLogInModal">sign up</p>
+          <p @click="openSignupModal"><span>sign up</span></p>
         </div>
       </form>
     </template>
@@ -54,44 +56,96 @@
   </button>
   <Modal v-show="signup.show" @close="onCloseSignup" title="Registartion">
     <template v-slot:body>
-      <form @submit.prevent="">
-        <label for="login">Login:</label>
-        <input
-          v-model="signup.email"
-          type="email"
-          name="login"
-          placeholder="Enter your email"
-        />
-        <label for="password">Password:</label>
-        <input
-          v-model="signup.password"
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-        <label for="confirmPassword">Confirm password:</label>
-        <input
-          v-model="signup.confirmPassword"
-          type="password"
-          name="confirmPassword"
-          placeholder="Enter your password again"
-        />
+      <form @submit.prevent="onSubmitSignUp">
+        <div class="form-item" :class="{ errorInput: v$.signup.email.$error }">
+          <label for="login">Login:</label>
+          <p class="errorText" v-if="v$.signup.email.required.$invalid">
+            Field is required
+          </p>
+          <p class="errorText" v-if="v$.signup.email.email.$invalid">
+            Email is't correct!
+          </p>
+          <input
+            :class="{ error: v$.signup.email.$error }"
+            v-model="signup.email"
+            type="email"
+            name="login"
+            placeholder="Enter your email"
+            @change="v$.signup.email.$touch"
+          />
+        </div>
+        <div
+          class="form-item"
+          :class="{ errorInput: v$.signup.password.$error }"
+        >
+          <label for="password">Password:</label>
+          <p class="errorText" v-if="v$.signup.password.required.$invalid">
+            Field is required
+          </p>
+          <p class="errorText" v-if="v$.signup.password.minLength.$invalid">
+            Password must be more than{{
+              v$.signup.password.minLength.$params.min
+            }}
+            symbols
+          </p>
+          <input
+            :class="{ error: v$.signup.password.$error }"
+            v-model="signup.password"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            @change="v$.signup.password.$touch"
+          />
+        </div>
+        <div
+          class="form-item"
+          :class="{ errorInput: v$.signup.confirmPassword.$error }"
+        >
+          <label for="confirmPassword">Confirm password:</label>
+          <p class="errorText" v-if="v$.signup.password.required.$invalid">
+            Field is required
+          </p>
+          <p
+            class="errorText"
+            v-if="v$.signup.confirmPassword.sameAsPassword.$invalid"
+          >
+            Passwords don't match
+          </p>
+          <input
+            :class="{ error: v$.signup.confirmPassword.$error }"
+            v-model="signup.confirmPassword"
+            type="password"
+            name="confirmPassword"
+            placeholder="Enter your password again"
+            @change="v$.signup.confirmPassword.$touch"
+          />
+        </div>
         <button class="btn btnPrimary">Sign up</button>
         <div class="form-notes">
           <p>Already have an account?</p>
-          <p @click="onSignUpModal">log in</p>
+          <p @click="openLoginModal"><span>log in</span></p>
         </div>
       </form>
+    </template>
+  </Modal>
+  <Modal
+    v-show="signup.thanks"
+    @close="signup.thanks = false"
+    title="Thank you"
+  >
+    <template v-slot:body>
+      <div class="form-notes">
+        <p>You just signed up!</p>
+        <p>Now you can <span @click="openLoginModal">log in</span></p>
+      </div>
     </template>
   </Modal>
 </template>
 
 <script>
 import useVuelidate from '@vuelidate/core'
-import { required, email, minLength } from '@vuelidate/validators'
-
+import { required, email, minLength, sameAs } from '@vuelidate/validators'
 import Modal from '@/components/UI/Modal.vue'
-
 export default {
   components: {
     Modal
@@ -110,7 +164,8 @@ export default {
         show: false,
         email: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        thanks: false
       }
     }
   },
@@ -136,7 +191,7 @@ export default {
         },
         confirmPassword: {
           required,
-          minLength: minLength(8)
+          sameAsPassword: sameAs(this.signup.password)
         }
       }
     }
@@ -161,13 +216,21 @@ export default {
       this.signup.show = false
       this.clearSignUp()
     },
-    onLogInModal() {
+    openSignupModal() {
       this.login.show = false
       this.signup.show = true
     },
-    onSignUpModal() {
+    openLoginModal() {
       this.signup.show = false
       this.login.show = true
+      this.signup.thanks = false
+    },
+    checkPassword() {
+      if (this.signup.password === this.signup.confirmPassword) {
+        this.signup.passwordMatch = true
+      } else {
+        this.signup.passwordMatch = false
+      }
     },
     async onSubmitLogin() {
       const isFormCorrect = await this.v$.login.$validate()
@@ -177,7 +240,7 @@ export default {
           password: this.login.password
         }
         console.log(user)
-        this.clearLogin()
+        this.onCloseLogin()
       }
     },
     async onSubmitSignUp() {
@@ -188,7 +251,8 @@ export default {
           password: this.signup.password
         }
         console.log(user)
-        this.clearSignUp()
+        this.onCloseSignup()
+        this.signup.thanks = true
       }
     }
   }
@@ -198,7 +262,7 @@ export default {
 <style lang="scss">
 .form-notes {
   margin-top: 20px;
-  & > :last-child {
+  & span {
     text-decoration: underline;
     color: #4468e0;
     cursor: pointer;
